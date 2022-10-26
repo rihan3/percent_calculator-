bf7741aa435d19f7d8d4d790b7b8bc7cd48b6348
Robaeit Hossan Rihan <99232414+rihan3@users.noreply.github.com>
2022-10-26 13:09:43 +0600

    Find out the percentage profit

---
+++ percentent of profit.py
@@ -0,0 +1,11 @@
+x = int(input("Input your purchese value: "))
+y = int(input("Input your Sales value: "))
+z = y - x 
+profit = float((z*100)/y)
+print("Your profit is ",z)
+print("Your percentance of Number: ",profit,"%")
+
+
+
+
+
